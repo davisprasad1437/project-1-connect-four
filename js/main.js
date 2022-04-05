94d4600c6dabd5c6907edd839b8da9e05540ad73
username <youremail@domain.com>
2022-04-05 10:31:36 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+let playerOneRed;
+let playerTwoBlue;
+let resetButton = document.getElementById('reset');
+
+
