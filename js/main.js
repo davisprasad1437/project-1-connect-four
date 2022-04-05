920be129b9dc22a2ec9a65bceaf2f147a68d731f
username <youremail@domain.com>
2022-04-05 15:28:27 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,92 @@ let playerOneRed;
 let playerTwoBlue;
 let resetButton = document.getElementById('reset');
 
+id01 = document.getElementById("01")
+id02 = document.getElementById("02")
+id03 = document.getElementById("03")
+id04 = document.getElementById("04")
+id05 = document.getElementById("05")
+id06 = document.getElementById("06")
+id07 = document.getElementById("07")
+id08 = document.getElementById("08")
+id09 = document.getElementById("09")
+id10 = document.getElementById("10")
+id11 = document.getElementById("11")
+id12 = document.getElementById("12")
+id13 = document.getElementById("13")
+id14 = document.getElementById("14")
+id15 = document.getElementById("15")
+id16 = document.getElementById("16")
+id17 = document.getElementById("17")
+id18 = document.getElementById("18")
+id19 = document.getElementById("19")
+id20 = document.getElementById("20")
+id21 = document.getElementById("21")
+id22 = document.getElementById("22")
+id23 = document.getElementById("23")
+id24 = document.getElementById("24")
+id25 = document.getElementById("25")
+id26 = document.getElementById("26")
+id27 = document.getElementById("27")
+id28 = document.getElementById("28")
+id29 = document.getElementById("29")
+id30 = document.getElementById("30")
+id31 = document.getElementById("31")
+id32 = document.getElementById("32")
+id33 = document.getElementById("33")
+id34 = document.getElementById("34")
+id35 = document.getElementById("35")
+id36 = document.getElementById("36")
+id37 = document.getElementById("37")
+id38 = document.getElementById("38")
+id39 = document.getElementById("39")
+id40 = document.getElementById("40")
+id41 = document.getElementById("41")
+id42 = document.getElementById("42")
+
 
 function handleResetButton() {
-    
+    id01.textContent = '';
+    id02.textContent = '';
+    id03.textContent = '';
+    id04.textContent = '';
+    id05.textContent = '';
+    id06.textContent = '';
+    id07.textContent = '';
+    id08.textContent = '';
+    id09.textContent = '';
+    id10.textContent = '';
+    id11.textContent = '';
+    id12.textContent = '';
+    id13.textContent = '';
+    id14.textContent = '';
+    id15.textContent = '';
+    id16.textContent = '';
+    id17.textContent = '';
+    id18.textContent = '';
+    id19.textContent = '';
+    id20textContent = '';
+    id21.textContent = '';
+    id22.textContent = '';
+    id23.textContent = '';
+    id24.textContent = '';
+    id25.textContent = '';
+    id26.textContent = '';
+    id27.textContent = '';
+    id28.textContent = '';
+    id29.textContent = '';
+    id30.textContent = '';
+    id31.textContent = '';
+    id32.textContent = '';
+    id33.textContent = '';
+    id34.textContent = '';
+    id35.textContent = '';
+    id36.textContent = '';
+    id37.textContent = '';
+    id38.textContent = '';
+    id39.textContent = '';
+    id40.textContent = '';
+    id41.textContent = '';
+    id42.textContent = '';
+
 }
