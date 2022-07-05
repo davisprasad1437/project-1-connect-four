9e437f5185db1b1cdfa24fa4d42d4f8fbd2a9849
Davis Prasad <fbifiji@gmail.com>
2022-07-05 13:09:04 -0700

    cant ge  JS to work

--- js/main.js
+++ js/main.js
@@ -1,161 +1,161 @@
-//---------- variables ----------//
+// //---------- variables ----------//
 
-let id01, id02, id03, id04, id05, id06, id07;
-let id08, id09, id10, id11, id12, id13, id14;
-let id15, id16, id17, id18, id019, id20, id21;
-let id22, id23, id24, id25, id26, id27, id28;
-let id29, id30, id31, id32, id33, id34, id35;
-let id36, id37, id38, id39, id40, id41, id42;
+// let id01, id02, id03, id04, id05, id06, id07;
+// let id08, id09, id10, id11, id12, id13, id14;
+// let id15, id16, id17, id18, id019, id20, id21;
+// let id22, id23, id24, id25, id26, id27, id28;
+// let id29, id30, id31, id32, id33, id34, id35;
+// let id36, id37, id38, id39, id40, id41, id42;
 
-let playerTwoBlue;
-let resetButton = document.getElementById('reset');
-
-
-//---------- element references ----------//
+// let playerTwoBlue;
+// let resetButton = document.getElementById('reset');
 
 
-id01 = document.getElementById("01")
-id02 = document.getElementById("02")
-id03 = document.getElementById("03")
-id04 = document.getElementById("04")
-id05 = document.getElementById("05")
-id06 = document.getElementById("06")
-id07 = document.getElementById("07")
-id08 = document.getElementById("08")
-id09 = document.getElementById("09")
-id10 = document.getElementById("10")
-id11 = document.getElementById("11")
-id12 = document.getElementById("12")
-id13 = document.getElementById("13")
-id14 = document.getElementById("14")
-id15 = document.getElementById("15")
-id16 = document.getElementById("16")
-id17 = document.getElementById("17")
-id18 = document.getElementById("18")
-id19 = document.getElementById("19")
-id20 = document.getElementById("20")
-id21 = document.getElementById("21")
-id22 = document.getElementById("22")
-id23 = document.getElementById("23")
-id24 = document.getElementById("24")
-id25 = document.getElementById("25")
-id26 = document.getElementById("26")
-id27 = document.getElementById("27")
-id28 = document.getElementById("28")
-id29 = document.getElementById("29")
-id30 = document.getElementById("30")
-id31 = document.getElementById("31")
-id32 = document.getElementById("32")
-id33 = document.getElementById("33")
-id34 = document.getElementById("34")
-id35 = document.getElementById("35")
-id36 = document.getElementById("36")
-id37 = document.getElementById("37")
-id38 = document.getElementById("38")
-id39 = document.getElementById("39")
-id40 = document.getElementById("40")
-id41 = document.getElementById("41")
-id42 = document.getElementById("42")
-
-//-------------event listeners for in div each Id's-----------//
-
-id01.addEventListener("click", handleButtonClick);
-id02.addEventListener("click", handleButtonClick);
-id03.addEventListener("click", handleButtonClick);
-id04.addEventListener("click", handleButtonClick);
-id05.addEventListener("click", handleButtonClick);
-id06.addEventListener("click", handleButtonClick);
-id07.addEventListener("click", handleButtonClick);
-id08.addEventListener("click", handleButtonClick);
-id09.addEventListener("click", handleButtonClick);
-id10.addEventListener("click", handleButtonClick);
-id11.addEventListener("click", handleButtonClick);
-id12.addEventListener("click", handleButtonClick);
-id13.addEventListener("click", handleButtonClick);
-id14.addEventListener("click", handleButtonClick);
-id15.addEventListener("click", handleButtonClick);
-id16.addEventListener("click", handleButtonClick);
-id17.addEventListener("click", handleButtonClick);
-id18.addEventListener("click", handleButtonClick);
-id19.addEventListener("click", handleButtonClick);
-id20.addEventListener("click", handleButtonClick);
-id21.addEventListener("click", handleButtonClick);
-id22.addEventListener("click", handleButtonClick);
-id23.addEventListener("click", handleButtonClick);
-id24.addEventListener("click", handleButtonClick);
-id25.addEventListener("click", handleButtonClick);
-id26.addEventListener("click", handleButtonClick);
-id27.addEventListener("click", handleButtonClick);
-id28.addEventListener("click", handleButtonClick);
-id29.addEventListener("click", handleButtonClick);
-id30.addEventListener("click", handleButtonClick);
-id31.addEventListener("click", handleButtonClick);
-id32.addEventListener("click", handleButtonClick);
-id33.addEventListener("click", handleButtonClick);
-id34.addEventListener("click", handleButtonClick);
-id35.addEventListener("click", handleButtonClick);
-id36.addEventListener("click", handleButtonClick);
-id37.addEventListener("click", handleButtonClick);
-id38.addEventListener("click", handleButtonClick);
-id39.addEventListener("click", handleButtonClick);
-id40.addEventListener("click", handleButtonClick);
-id41.addEventListener("click", handleButtonClick);
-id42.addEventListener("click", handleButtonClick);
-
-resetButton.addEventListener('click', handleResetButton);
-
-// ----------functions---------//
+// //---------- element references ----------//
 
 
-function handleButtonClick(event) {
-    if (event.target.textContent===''){event.target.textContent = playerTurn === 0 ? ('X'): ('O');
-    playerTurn = playerTurn === 0 ? 1: 0;
-    }
-  }
+// id01 = document.getElementById("01")
+// id02 = document.getElementById("02")
+// id03 = document.getElementById("03")
+// id04 = document.getElementById("04")
+// id05 = document.getElementById("05")
+// id06 = document.getElementById("06")
+// id07 = document.getElementById("07")
+// id08 = document.getElementById("08")
+// id09 = document.getElementById("09")
+// id10 = document.getElementById("10")
+// id11 = document.getElementById("11")
+// id12 = document.getElementById("12")
+// id13 = document.getElementById("13")
+// id14 = document.getElementById("14")
+// id15 = document.getElementById("15")
+// id16 = document.getElementById("16")
+// id17 = document.getElementById("17")
+// id18 = document.getElementById("18")
+// id19 = document.getElementById("19")
+// id20 = document.getElementById("20")
+// id21 = document.getElementById("21")
+// id22 = document.getElementById("22")
+// id23 = document.getElementById("23")
+// id24 = document.getElementById("24")
+// id25 = document.getElementById("25")
+// id26 = document.getElementById("26")
+// id27 = document.getElementById("27")
+// id28 = document.getElementById("28")
+// id29 = document.getElementById("29")
+// id30 = document.getElementById("30")
+// id31 = document.getElementById("31")
+// id32 = document.getElementById("32")
+// id33 = document.getElementById("33")
+// id34 = document.getElementById("34")
+// id35 = document.getElementById("35")
+// id36 = document.getElementById("36")
+// id37 = document.getElementById("37")
+// id38 = document.getElementById("38")
+// id39 = document.getElementById("39")
+// id40 = document.getElementById("40")
+// id41 = document.getElementById("41")
+// id42 = document.getElementById("42")
+
+// //-------------event listeners for in div each Id's-----------//
+
+// id01.addEventListener("click", handleButtonClick);
+// id02.addEventListener("click", handleButtonClick);
+// id03.addEventListener("click", handleButtonClick);
+// id04.addEventListener("click", handleButtonClick);
+// id05.addEventListener("click", handleButtonClick);
+// id06.addEventListener("click", handleButtonClick);
+// id07.addEventListener("click", handleButtonClick);
+// id08.addEventListener("click", handleButtonClick);
+// id09.addEventListener("click", handleButtonClick);
+// id10.addEventListener("click", handleButtonClick);
+// id11.addEventListener("click", handleButtonClick);
+// id12.addEventListener("click", handleButtonClick);
+// id13.addEventListener("click", handleButtonClick);
+// id14.addEventListener("click", handleButtonClick);
+// id15.addEventListener("click", handleButtonClick);
+// id16.addEventListener("click", handleButtonClick);
+// id17.addEventListener("click", handleButtonClick);
+// id18.addEventListener("click", handleButtonClick);
+// id19.addEventListener("click", handleButtonClick);
+// id20.addEventListener("click", handleButtonClick);
+// id21.addEventListener("click", handleButtonClick);
+// id22.addEventListener("click", handleButtonClick);
+// id23.addEventListener("click", handleButtonClick);
+// id24.addEventListener("click", handleButtonClick);
+// id25.addEventListener("click", handleButtonClick);
+// id26.addEventListener("click", handleButtonClick);
+// id27.addEventListener("click", handleButtonClick);
+// id28.addEventListener("click", handleButtonClick);
+// id29.addEventListener("click", handleButtonClick);
+// id30.addEventListener("click", handleButtonClick);
+// id31.addEventListener("click", handleButtonClick);
+// id32.addEventListener("click", handleButtonClick);
+// id33.addEventListener("click", handleButtonClick);
+// id34.addEventListener("click", handleButtonClick);
+// id35.addEventListener("click", handleButtonClick);
+// id36.addEventListener("click", handleButtonClick);
+// id37.addEventListener("click", handleButtonClick);
+// id38.addEventListener("click", handleButtonClick);
+// id39.addEventListener("click", handleButtonClick);
+// id40.addEventListener("click", handleButtonClick);
+// id41.addEventListener("click", handleButtonClick);
+// id42.addEventListener("click", handleButtonClick);
+
+// resetButton.addEventListener('click', handleResetButton);
+
+// // ----------functions---------//
 
 
-function handleResetButton() {
-    id01.textContent = '';
-    id02.textContent = '';
-    id03.textContent = '';
-    id04.textContent = '';
-    id05.textContent = '';
-    id06.textContent = '';
-    id07.textContent = '';
-    id08.textContent = '';
-    id09.textContent = '';
-    id10.textContent = '';
-    id11.textContent = '';
-    id12.textContent = '';
-    id13.textContent = '';
-    id14.textContent = '';
-    id15.textContent = '';
-    id16.textContent = '';
-    id17.textContent = '';s
-    id18.textContent = '';
-    id19.textContent = '';
-    id20.textContent = '';
-    id21.textContent = '';
-    id22.textContent = '';
-    id23.textContent = '';
-    id24.textContent = '';
-    id25.textContent = '';
-    id26.textContent = '';
-    id27.textContent = '';
-    id28.textContent = '';
-    id29.textContent = '';
-    id30.textContent = '';
-    id31.textContent = '';
-    id32.textContent = '';
-    id33.textContent = '';
-    id34.textContent = '';
-    id35.textContent = '';
-    id36.textContent = '';
-    id37.textContent = '';
-    id38.textContent = '';
-    id39.textContent = '';
-    id40.textContent = '';
-    id41.textContent = '';
-    id42.textContent = '';
+// function handleButtonClick(event) {
+//     if (event.target.textContent===''){event.target.textContent = playerTurn === 0 ? ('X'): ('O');
+//     playerTurn = playerTurn === 0 ? 1: 0;
+//     }
+//   }
 
-}
+
+// function handleResetButton() {
+//     id01.textContent = '';
+//     id02.textContent = '';
+//     id03.textContent = '';
+//     id04.textContent = '';
+//     id05.textContent = '';
+//     id06.textContent = '';
+//     id07.textContent = '';
+//     id08.textContent = '';
+//     id09.textContent = '';
+//     id10.textContent = '';
+//     id11.textContent = '';
+//     id12.textContent = '';
+//     id13.textContent = '';
+//     id14.textContent = '';
+//     id15.textContent = '';
+//     id16.textContent = '';
+//     id17.textContent = '';s
+//     id18.textContent = '';
+//     id19.textContent = '';
+//     id20.textContent = '';
+//     id21.textContent = '';
+//     id22.textContent = '';
+//     id23.textContent = '';
+//     id24.textContent = '';
+//     id25.textContent = '';
+//     id26.textContent = '';
+//     id27.textContent = '';
+//     id28.textContent = '';
+//     id29.textContent = '';
+//     id30.textContent = '';
+//     id31.textContent = '';
+//     id32.textContent = '';
+//     id33.textContent = '';
+//     id34.textContent = '';
+//     id35.textContent = '';
+//     id36.textContent = '';
+//     id37.textContent = '';
+//     id38.textContent = '';
+//     id39.textContent = '';
+//     id40.textContent = '';
+//     id41.textContent = '';
+//     id42.textContent = '';
+
+// }
